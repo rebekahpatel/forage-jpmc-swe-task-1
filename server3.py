9f0783ec00db1fcacca0aed410ca36296a9b5121
Rebekah <rebapatel@gmail.com>
2023-08-20 19:53:20 -0700

    Task 1: Setting up Local Env & Making Changes

--- server3.py
+++ server3.py
@@ -18,6 +18,7 @@
 #  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 #  DEALINGS IN THE SOFTWARE.
 
+
 import csv
 # from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 import http.server
